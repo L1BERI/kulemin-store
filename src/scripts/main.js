9e48d2e0000dc35c9fdc7c5974951485721fe96e
unknown <kael1nvoker@mail.ru>
2024-08-06 12:18:42 +0300

    Created testimonials section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 import { burgerOpen } from './burger';
 import './cards';
 import './collection';
+import './testimonials';
 import { mouseMoveTranform } from './mousemove';
 import '../scss/main.scss';
 
---
+++ src/scripts/testimonials.js
@@ -0,0 +1,34 @@
+import Swiper from 'swiper';
+import { Autoplay, Scrollbar } from 'swiper/modules';
+
+import 'swiper/css';
+
+const testimonialsSwiper = new Swiper('.testimonials__swiper',{
+    modules: [Autoplay, Scrollbar],
+    slidesPerView: 1.2,
+    spaceBetween: 35,
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        draggable: true,
+        dragSize: 76,
+    },
+    autoplay:{
+        delay: 3000,
+    },
+    breakpoints:{
+        1200:{
+            slidesPerView: 3.3,
+            scrollbar: {
+                dragSize: 176,
+            },
+        },
+        576:{
+            slidesPerView: 2.3,
+            
+        },
+        456:{
+            slidesPerView:1.4,
+        },
+    }
+
+})
